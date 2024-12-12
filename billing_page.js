885efa10d93b75ac4056b96a9f5edeac2481d31c
Nethmi4 <cb015790@students.apiit.lk>
2024-12-12 21:19:16 +0530

    Delete billing_page.js

--- billing_page.js
+++
@@ -1,9 +0,0 @@
-let pay = document.getElementById('pay');
-
-function f_alert(){
-    alert('Thank you for your purchase!')
-}
-
-pay.addEventListener('click', f_alert);
-
-// alert(`Thank you for your purchase! Your order: ${orderSummary}. Total: ${grandTotal}. Delivery Date: ${deliveryDate.toDateString()}`);
